925790f81c88e4c85cf2d1dabc1cdcba02d1e22f
beka.dev <bigsamsa3@mail.ru>
2023-03-14 22:11:22 +0600

    commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {combineReducers, createStore} from 'redux'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+let initialState = {items: [{name: 'Dimych'}, {name: 'Ignat'}]}
+const usersReducer = (state = initialState, action: any) => {
+    return state
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const store = createStore(combineReducers({
+    users: usersReducer
+}))
+
+store.subscribe(() => {
+    console.log('state changed')
+})
+
+store.dispatch({type: 'ANY'})
+
+// Что нужно написать вместо XXX, чтобы в консоли увидеть 'state changed'?
